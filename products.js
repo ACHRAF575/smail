0e270182a22baa4f9ed933f3065110d66ee05717
ACHRAF575 <limouriachraf07@gmail.com>
2026-01-22 13:24:23 -0800

    Add files via upload

---
+++ products.js
@@ -0,0 +1,101 @@
+window.storeData = {
+  "config": {
+    "storeName": "Golden Elegance",
+    "heroImage": "https://images.unsplash.com/photo-1566174053879-31528523f8ae?ixlib=rb-4.0.3&auto=format&fit=crop&w=1920&q=80",
+    "heroTitle": "Modest Luxury",
+    "heroSubtitle": "Explore our exclusive collection",
+    "phone": "+1 234 567 890",
+    "email": "contact@goldenelegance.com",
+    "whatsapp": "0690666040",
+    "instagram": "mrbeast",
+    "facebook": "your_page_id",
+    "address": "123 Fashion Street, City Center"
+  },
+  "products": [
+    {
+      "id": 1,
+      "name": "Royal Gold Kaftan",
+      "category": "gowns",
+      "image": "https://images.unsplash.com/photo-1595777457583-95e059d581b8?w=500&q=80",
+      "description": "Embellished Detailing",
+      "price": ""
+    },
+    {
+      "id": 2,
+      "name": "Premium Chiffon Hijab",
+      "category": "fabrics",
+      "image": "https://images.unsplash.com/photo-1584917865442-de89df76afd3?w=500&q=80",
+      "description": "Available in 20+ Colors",
+      "price": "$15.00"
+    },
+    {
+      "id": 1769039092113,
+      "name": "gfh",
+      "category": "fabrics",
+      "image": "image.png",
+      "description": "",
+      "price": ""
+    },
+    {
+      "id": 1769039096817,
+      "name": "fdfd",
+      "category": "fabrics",
+      "image": "image.png",
+      "description": "",
+      "price": ""
+    },
+    {
+      "id": 1769039101201,
+      "name": "gfgf",
+      "category": "fabrics",
+      "image": "image.png",
+      "description": "",
+      "price": ""
+    },
+    {
+      "id": 1769039106345,
+      "name": "fgfg",
+      "category": "fabrics",
+      "image": "image.png",
+      "description": "",
+      "price": ""
+    },
+    {
+      "id": 1769039111465,
+      "name": "jhgj",
+      "category": "fabrics",
+      "image": "image.png",
+      "description": "",
+      "price": ""
+    },
+    {
+      "id": 1769080963542,
+      "name": "dfgV",
+      "category": "fabrics",
+      "badge": "new",
+      "image": "image.png",
+      "description": "",
+      "price": ""
+    }
+  ],
+  "stories": [
+    {
+      "id": 1769089247243,
+      "thumb": "image.png",
+      "video": "vid.mp4",
+      "seen": false
+    },
+    {
+      "id": 1769113065817,
+      "thumb": "image.png",
+      "video": "vid1.mp4",
+      "seen": false
+    },
+    {
+      "id": 1769113085304,
+      "thumb": "image.png",
+      "video": "vid2.mp4",
+      "seen": false
+    }
+  ]
+};
